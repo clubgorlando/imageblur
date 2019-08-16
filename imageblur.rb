3f6cff65c3b5f22cc7bac57853b88efa0c8bf248
clubgorlando <eric.gorland@gmail.com>
2019-08-16 19:28:21 +0000

    initial commit

---
+++ imageblur.rb
@@ -0,0 +1,25 @@
+class Image
+  attr_accessor :image
+
+  def initialize(image_data)
+    self.image = Array.new {Array.new}
+    self.image = image_data
+  end
+
+  def output_image
+    self.image.each do |row|
+      puts row.each { |image| image }.join()
+    end
+
+  end
+
+end
+
+image = Image.new([
+  [0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0], 
+])
+
+image.output_image
